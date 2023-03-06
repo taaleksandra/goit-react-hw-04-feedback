99d8c95cd19cf0228b66a02fa9aef929657e2261
taaleksandra <olatarnas@gmail.com>
2023-03-06 11:31:04 +0100

    corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,64 @@
-import { Feedback } from './Feedback/Feedback';
+import React, { Component } from 'react';
+import clsx from 'clsx';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+import css from '../components/Feedback/Feedback.module.css';
+
+import { Section } from '../components/Section/Section';
+import { Statistics } from '../components/Statistics/Statistics';
+import { FeedbackOptions } from '../components/Feedback/FeedbackOptions';
+import { Notification } from '../components/Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = evt => {
+    const btnName = evt.currentTarget.textContent;
+    this.setState(prev => {
+      return { [btnName]: prev[btnName] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
+    const positiveFeedback = Math.round(
+      (this.state.good * 100) / totalFeedback
+    );
+    return totalFeedback === 0 ? totalFeedback : positiveFeedback;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div className={clsx(css.feedback)}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification msg="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
